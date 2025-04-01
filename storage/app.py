71a7d18666ce2482e298b7bcb8bf73d1a4a2c35b
Rylan Raj <rylan.raj@gmail.com>
2025-04-01 12:16:04 -0700

    lab11

--- storage/app.py
+++ storage/app.py
@@ -76,51 +76,58 @@ def get_humidities(session, start_timestamp, end_timestamp):
 
     return results
 
-@use_db_session
-def process_messages(session):
+def process_messages():
     """ Process event messages """
-    logger.info("Connecting to Kafka")
-    event_hostname = app_config["events"]["hostname"]
-    event_port = app_config["events"]["port"]
-    hostname = f"{event_hostname}:{event_port}" # localhost:9092
-    client = KafkaClient(hosts=hostname)
-    topic = client.topics[str.encode(app_config["events"]["topic"])]
-    # Create a consume on a consumer group, that only reads new messages
-    # (uncommitted messages) when the service re-starts (i.e., it doesn't
-    # read all the old messages from the history in the message queue).
-    consumer = topic.get_simple_consumer(consumer_group=b'event_group',
-    reset_offset_on_start=False,
-    auto_offset_reset=OffsetType.LATEST)
-    # This is blocking - it will wait for a new message
-    for msg in consumer:
-        msg_str = msg.value.decode('utf-8')
-        msg = json.loads(msg_str)
-        logger.info("Message: %s" % msg)
-        payload = msg["payload"]
-        if msg["type"] == "temperature": # Change this to your event type
-            # Store the event1 (i.e., the payload) to the DB
-            obj = TemperatureReport(
-                device_id=payload["device_id"],
-                temperature=payload["temperature"],
-                location=payload["location"],
-                timestamp=datetime.strptime(payload["timestamp"], "%Y-%m-%dT%H:%M:%S"),
-                trace_id=payload["trace_id"]
-            )
-            session.add(obj)
+    session = make_session()
+    try:
+        logger.info("Connecting to Kafka")
+        event_hostname = app_config["events"]["hostname"]
+        event_port = app_config["events"]["port"]
+        hostname = f"{event_hostname}:{event_port}"  # localhost:9092
+        client = KafkaClient(hosts=hostname)
+        topic = client.topics[str.encode(app_config["events"]["topic"])]
+        # Create a consume on a consumer group, that only reads new messages
+        # (uncommitted messages) when the service re-starts (i.e., it doesn't
+        # read all the old messages from the history in the message queue).
+        consumer = topic.get_simple_consumer(
+            consumer_group=b'event_group',
+            reset_offset_on_start=False,
+            auto_offset_reset=OffsetType.LATEST
+        )
+        # This is blocking - it will wait for a new message
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+            logger.info("Message: %s" % msg)
+            payload = msg["payload"]
+            if msg["type"] == "temperature":  # Change this to your event type
+                # Store the event1 (i.e., the payload) to the DB
+                obj = TemperatureReport(
+                    device_id=payload["device_id"],
+                    temperature=payload["temperature"],
+                    location=payload["location"],
+                    timestamp=datetime.strptime(payload["timestamp"], "%Y-%m-%dT%H:%M:%S"),
+                    trace_id=payload["trace_id"]
+                )
+                session.add(obj)
 
-        elif msg["type"] == "humidity": # Change this to your event type
-            # Store the event2 (i.e., the payload) to the DB
-            obj = HumidityReport(
-                device_id=payload["device_id"],
-                humidity=payload["humidity"],
-                location=payload["location"],
-                timestamp=datetime.strptime(payload["timestamp"], "%Y-%m-%dT%H:%M:%S"),
-                trace_id=payload["trace_id"]
-            )
-            session.add(obj)
-        # Commit the new message as being read
-        session.commit()
-        logger.info("Stored event %s with trace ID %s" % (msg["type"], payload["trace_id"]))
+            elif msg["type"] == "humidity":  # Change this to your event type
+                # Store the event2 (i.e., the payload) to the DB
+                obj = HumidityReport(
+                    device_id=payload["device_id"],
+                    humidity=payload["humidity"],
+                    location=payload["location"],
+                    timestamp=datetime.strptime(payload["timestamp"], "%Y-%m-%dT%H:%M:%S"),
+                    trace_id=payload["trace_id"]
+                )
+                session.add(obj)
+            # Commit the new message as being read
+            session.commit()
+            logger.info("Stored event %s with trace ID %s" % (msg["type"], payload["trace_id"]))
+    except Exception as e:
+        logger.error(f"Error processing Kafka messages: {e}")
+    finally:
+        session.close()
 
 def setup_kafka_thread():
     t1 = Thread(target=process_messages)
