1da87c1444b4178654d84eae6436d1552e20e83d
Rylan Raj <rylan.raj@gmail.com>
2025-04-01 09:56:24 -0700

    lab11

--- analyzer/app.py
+++ analyzer/app.py
@@ -77,4 +77,4 @@ app.add_middleware(
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
-    app.run(port=8110, host="0.0.0.0")
+    app.run(port=8110, host="0.0.0.0", base_path="/analyzer")
