b18038d5ed7ab17024ee4e0fb1709e8c98b96616
Rylan Raj <rylan.raj@gmail.com>
2025-04-08 10:54:17 -0700

    change hostnaes

--- storage/app.py
+++ storage/app.py
@@ -30,7 +30,7 @@ database = datastore["db"]
 
 engine = create_engine(
     f"mysql+pymysql://{user}:{password}@{hostname}:{port}/{database}",
-    pool_size=10,
+    pool_size=20,
     pool_recycle=3600,
     pool_pre_ping=True
 )
